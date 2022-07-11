aef1240501ed2e2dc078aa952a3cf538e5e4d407
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-11 15:36:44 +0200

    added dynamic inputs, removed console.log

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 
-console.log('vue ok',Vue);
-
 const root = new Vue({
     el: '#display',
     data: {
